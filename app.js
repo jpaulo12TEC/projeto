e08a0e73a6c90e218eb017fe98a09233ba8ec2fd
João  SME <compras@12tec.com.br>
2025-01-12 13:19:14 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,22 @@
+let numeroSecreto = parseInt(4);
+let chute;
+let tentativas = 1
+
+alert('Bem vindo ao jogo do número Secreto!!');
+
+while (chute != numeroSecreto){
+    chute = parseInt(prompt('Escolha um número entre 1 e 10'));
+
+    if(chute == numeroSecreto){
+     alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+    } else {
+            if (chute > numeroSecreto) {
+                alert(`o número secreto é menor que ${chute}`);
+        } else {
+                alert(`o número secreto é maior que ${chute}`);
+    }
+
+    tentativa++;
+}
+} 
+
